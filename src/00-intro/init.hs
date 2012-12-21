52291ab26554063d78ff6ed677a574a0d04ac8e8
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2012-12-21 13:55:43 +0100

    Chapter 2 - some basic functions to help in manipulating data.

--- src/00-intro/init.hs
+++ src/00-intro/init.hs
@@ -34,10 +34,6 @@ rqsort (x:xs) = rqsort (filter (>= x) xs) ++ [x] ++ rqsort (filter (< x) xs)
 -- rqsort [3, 1, 7, 9, 6]
 -- [9,7,6,3,1]
 
--- take
--- take 2 [1,3,4,5,8]
--- [1,3]
-
 pdt :: Num a => [a] -> a
 pdt [] = 1
 pdt (x:xs) = x * pdt xs
@@ -48,3 +44,45 @@ pdt (x:xs) = x * pdt xs
 uqsort :: Ord a => [a] -> [a]
 uqsort [] = []
 uqsort (x:xs) = rqsort (filter (< x) xs) ++ [x] ++ rqsort (filter (> x) xs)
+
+-- head [3,4,5]
+-- 3
+
+-- head [1]
+-- 1
+
+-- head []
+-- error
+
+-- tail [3,4,5]
+-- [4.5]
+
+-- tail [1]
+-- []
+
+-- tail []
+-- error
+
+-- [1,2,3,98,99] !! 3
+-- 98
+
+-- take 2 [1,3,4,5,8]
+-- [1,3]
+
+-- drop 2 [1,3,4,5,8]
+-- [4,5,8]
+
+-- length [1,3,5,6]
+-- 4
+
+-- sum [1,3,5,6]
+-- 15
+
+-- product [1,3,5,6]
+-- 90
+
+-- [1,2,3] ++ [4,5]
+-- [1,2,3,4,5]
+
+-- reverse [1,2,3,4,5]
+-- [5,4,3,2,1]
