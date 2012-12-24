1dc91fb462ef547a8b87970457d8ab29509ac9b7
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2012-12-24 14:18:09 +0100

    Multiple implementation of last using amongst drop, head, length, !! functions.

--- src/02-chap2/ch2.hs
+++ src/02-chap2/ch2.hs
@@ -3,3 +3,17 @@ n = a `div` length xs
     where
       a  = 10
       xs = [1, 2, 3, 4, 5]
+
+-- implementation of last
+last1 :: [a] -> a
+last1 xs = head (reverse xs)
+
+last2 :: [a] -> a
+last2 xs = xs !! i
+           where
+             i = length xs - 1
+
+last3 :: [a] -> a
+last3 xs = head (drop i xs)
+           where
+             i = length xs - 1
