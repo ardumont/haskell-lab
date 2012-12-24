13f39577591e77684e8275dec1795b586f2cb644
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2012-12-24 14:58:34 +0100

    drop 1 <=> tail

--- src/02-chap2/ch2.hs
+++ src/02-chap2/ch2.hs
@@ -1,4 +1,5 @@
 module Ch2 where
+-- fix problems
 n = a `div` length xs
     where
       a  = 10
@@ -15,3 +16,13 @@ last3 :: [a] -> a
 last3 xs = head (drop i xs)
            where
              i = length xs - 1
+
+-- implementation of init: removes the last element from a non-empty list
+init1 :: [a] -> [a]
+init1 xs = take (length xs - 1) xs
+
+init2 :: [a] -> [a]
+init2 xs  = reverse (drop 1 (reverse xs))
+
+init3 :: [a] -> [a]
+init3 xs  = reverse (tail (reverse xs))
