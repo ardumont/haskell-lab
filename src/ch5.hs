ef28c366132fb7f49ae0d4a2641fcb466b356783
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2012-12-29 21:13:04 +0100

    scalarproduct

--- src/ch5.hs
+++ src/ch5.hs
@@ -301,3 +301,9 @@ positions2 v vs = find v valpos
 -- [2,6]
 -- *Ch5> positions2 1 [0,1,3,4,2,1,3]
 -- [2,6]
+
+scalarproduct :: [Int] -> [Int] -> Int
+scalarproduct xs ys = sum [x * y | (x, y) <- zip xs ys]
+
+-- *Ch5> scalarproduct [1,2,3] [4,5,6]
+-- 32
