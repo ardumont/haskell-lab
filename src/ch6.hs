f0f0b498260f41a60f31444b1913692ff2a92b6d
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2012-12-31 17:53:56 +0100

    (*) definition.

--- src/ch6.hs
+++ src/ch6.hs
@@ -2,4 +2,10 @@ module Ch6 where
 
 factorial :: Int -> Int
 factorial 0 = 1
-factorial n = n * factorial (n - 1)
+factorial n = n Prelude.* factorial (n - 1)
+-- factorial (n+1) = (n+1) * factorial n -- does not work, do not know why
+
+(*) :: Int -> Int -> Int
+_ * 0 = 0
+m * n = m + m Ch6.* (n - 1)
+--m * (n + 1) = m + m * n -- does not work, do not know why
