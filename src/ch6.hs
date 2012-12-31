c13be189a75202bf335553514e7214fbfffebab6
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2012-12-31 17:55:59 +0100

    product

--- src/ch6.hs
+++ src/ch6.hs
@@ -2,10 +2,14 @@ module Ch6 where
 
 factorial :: Int -> Int
 factorial 0 = 1
-factorial n = n Prelude.* factorial (n - 1)
+factorial n = n * factorial (n - 1)
 -- factorial (n+1) = (n+1) * factorial n -- does not work, do not know why
 
-(*) :: Int -> Int -> Int
-_ * 0 = 0
-m * n = m + m Ch6.* (n - 1)
+-- (*) :: Int -> Int -> Int
+-- _ * 0 = 0
+-- m * n = m + m Ch6.* (n - 1)
 --m * (n + 1) = m + m * n -- does not work, do not know why
+
+pdt :: [Int] -> Int
+pdt [] = 1
+pdt (x:xs) = x * pdt xs
