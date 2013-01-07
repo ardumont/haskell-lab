d6654614f450c45122b18beaa5f56a080a522cdc
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2013-01-07 08:49:35 +0100

    Simpler writing about currify/uncurrify

--- src/ch7_2.hs
+++ src/ch7_2.hs
@@ -9,9 +9,18 @@ cur :: ((a,b) -> c) -> a -> b -> c
 cur f = \x -> \y -> f (x,y)
 
 -- here are examples:
+-- *Ch7_2> (cur add) 1 2
+-- 3
 -- *Ch7_2> ((cur add) 1) 2
 -- 3
--- *Ch7_2> (cur add) 1 2
+
+-- which can be put simply
+currify :: ((a,b) -> c) -> a -> b -> c
+currify f x y = f (x,y)
+
+-- *Ch7_2> (currify add) 1 2
+-- 3
+-- *Ch7_2> ((currify add) 1) 2
 -- 3
 
 uncur :: (a -> b -> c) -> (a, b) -> c
@@ -22,4 +31,11 @@ uncur f = \ (x, y) -> (f x y)
 -- *Ch7_2> (cur add) 1 2
 -- 3
 -- *Ch7_2> uncur (cur add) (1,2)
+-- 3
+
+-- which can be put simply
+uncurrify :: (a -> b -> c) -> (a, b) -> c
+uncurrify f (x, y) = f x y
+
+-- *Ch7_2> uncurrify (currify add) (1,2)
 -- 3
