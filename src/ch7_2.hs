ac6fc332d023fb3e642d49ce925e993c87e9d531
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2013-01-07 17:28:35 +0100

    unfold + int2bin using unfold

--- src/ch7_2.hs
+++ src/ch7_2.hs
@@ -39,3 +39,22 @@ uncurrify f (x, y) = f x y
 
 -- *Ch7_2> uncurrify (currify add) (1,2)
 -- 3
+
+unfold :: (t -> Bool) -> (t -> a) -> (t -> t) -> t -> [a]
+unfold p h t x | p x = []
+               | otherwise = h x : unfold p h t (t x)
+
+type Bit = Int
+
+-- int2bin :: Int -> [Bit]
+-- int2bin 0 = []
+-- int2bin n = int2bin (n `div` 2) ++ [n `mod` 2]
+
+-- then
+int2bin :: Int -> [Bit]
+int2bin = unfold (== 0) (`mod` 2) (`div` 2)
+
+-- *Ch7_2> int2bin 20
+-- [0,0,1,0,1]
+-- *Ch7_2> int2bin 128
+-- [0,0,0,0,0,0,0,1]
