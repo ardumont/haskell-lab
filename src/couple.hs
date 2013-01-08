5f21616de86ad9b1b31ee4b049e78b68528bc0a9
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2013-01-08 02:20:40 +0100

    quickcheck tryout

--- src/couple.hs
+++ src/couple.hs
@@ -1,7 +1,14 @@
 module Couple where
+import Data.Char
+import Test.QuickCheck
 
 -- unique couple (a,b) such that a*b = n
 -- As the multiplication is commutative, we consider (a,b) == (b,a)
 
-couple :: Int -> [(Int,Int)]
-couple n = [(a,b) | a <- [1..n], b <- [1..a], a * b == n]
+couple :: Int -> [(Int, Int)]
+couple n = [(a,b) | a <- [2..n], b <- [1..a], a * b == n]
+
+-- *Couple> quickCheck (\ x -> all (\ (a,b) -> b <= a) (couple x))
+--   C-c C-c*** Failed! Exception: 'user interrupt' (after 27 tests):
+-- 47540
+-- took too long
