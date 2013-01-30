d78705bb69b9dd53f813bc2eb73aabea4ee4ce31
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2013-01-30 09:41:15 +0100

    return parser

--- src/ch8.hs
+++ src/ch8.hs
@@ -1,3 +1,13 @@
 module Parsers where
 
 type Parser a = String -> [(a, String)]
+
+-- basic parsers
+
+-- return parser
+
+r :: a -> Parser a
+r v = \ inp -> [(v, inp)]
+
+-- *Parsers> (r "value") "input"
+-- [("value","input")]
