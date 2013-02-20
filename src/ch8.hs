40f47c6d6eb9d17eaf4a8a59a135cbd13ab5221f
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2013-02-20 09:06:38 +0100

    failure parser

--- src/ch8.hs
+++ src/ch8.hs
@@ -4,16 +4,15 @@ type Parser a = String -> [(a, String)]
 
 -- basic parsers
 
--- return parser
--- always succeeds by returning the result value v without consuming
--- the input
+-- return parser: always succeeds by returning the result value v without
+-- consuming the input
 ret :: a -> Parser a
 ret v = \ input -> [(v, input)]
 
 -- *Parsers> ret 1 "input-string-without-consuming"
 -- [(1,"input-string-without-consuming")]
 
--- failure parser
+-- failure parser: always fails whatever the input
 failure :: Parser a
 failure = \ _ -> []
 
