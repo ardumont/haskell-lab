f3064e487efbbc31bec19241d3c285861285592d
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2013-04-15 08:02:45 +0200

    Renaming module

---
+++ src/Monade.hs
@@ -0,0 +1,50 @@
+module Monade where
+
+-- getChar :: IO Char
+-- putChar :: Char -> IO ()
+-- return :: a -> IO a
+
+getL :: IO String
+getL = do x <- getChar
+          if x == '\n'
+            then return []
+            else (do xs <- getL
+                     return (x:xs))
+
+putS :: String -> IO ()
+putS s = case s of
+  []     -> return ()
+  (x:xs) -> (do putChar x
+                putS xs)
+
+putSLn :: String -> IO ()
+putSLn s = do putS s
+              putChar '\n'
+
+strlen :: IO ()
+strlen = do putSLn "Give me some input, please:"
+            inp <- getL
+            putSLn ("Length: " ++ (show (length inp)) ++ " characters.")
+
+beep :: IO ()
+beep = putStr "\BEL"
+
+cls :: IO ()
+cls = putStr "\ESC[2J"
+
+type Pos = (Int, Int)
+
+goto :: Pos -> IO ()
+goto (x, y) = putS ("\ESC[" ++ show y ++ ";" ++ show x ++ "H")
+
+writeat :: Pos -> String -> IO ()
+writeat p s = do goto p
+                 putSLn s
+
+seqn :: [IO a] -> IO ()
+seqn [] = return ()
+seqn (a:as) = do a
+                 seqn as
+
+putS2 :: String -> IO ()
+putS2 s = seqn [ putChar x | x <- s ]
