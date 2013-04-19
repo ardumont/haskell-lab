f8d73493dc3a2bdc5a062d91f75fd8d0e2bc4d9b
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2013-04-19 23:29:39 +0200

    Simplifying using getContents.

--- src/io/capslock.hs
+++ src/io/capslock.hs
@@ -1,9 +1,17 @@
 module Capslock where
 
-import Control.Monad (forever)
 import GHC.Unicode (toUpper)
 
 main :: IO ()
-main = forever $
-            do x <- getLine
-               putStrLn (map toUpper x)
+main = do x <- getContents
+          putStrLn (map toUpper x)
+
+-- getContents :: IO String
+
+-- cat resources/quotes | runhaskell capslock
+-- I THINK PROGRAMMERS HAVE BECOME
+-- INURED TO INCIDENTAL COMPLEXITY...
+-- WHEN THEY ENCOUNTER COMPLEXITY,
+-- THEY CONSIDER IT A CHALLENGE TO OVERCOME,
+-- RATHER THAN AN OBSTACLE TO REMOVE.
+-- OVERCOMING COMPLEXITY ISN'T WORK, IT'S WASTE.
