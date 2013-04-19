b044baf482ee099cd92d2cd1cccb2c20810783c0
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2013-04-19 09:56:56 +0200

    infiniteRead reverse the words from the sentence

--- src/io/hw.hs
+++ src/io/hw.hs
@@ -9,5 +9,19 @@ infiniteRead :: IO ()
 infiniteRead = do line <- getLine
                   if null line
                     then return ()
-                    else do putStrLn $ reverse line
+                    else do putStrLn $ reverseWords line
                             infiniteRead
+
+breakWords :: String -> [String]
+breakWords [] = []
+breakWords s = case (break (== ' ') s) of
+  (x, xs) -> x : (breakWords . drop 1) xs
+
+-- *Main> breakWords "this is a test"
+-- ["this","is","a","test"]
+
+reverseWords :: String -> String
+reverseWords = unwords . reverse . breakWords
+
+-- *Main> reverseWords "this is to be reversed"
+-- "reversed be to is this"
