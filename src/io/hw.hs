3b0be6e6ce7b61b9d5e222e3d31d3fad11b456f3
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2013-04-19 09:32:38 +0200

    infiniteRead

--- src/io/hw.hs
+++ src/io/hw.hs
@@ -4,3 +4,10 @@ main :: IO ()
 main = do putStrLn "Hello, what's your name?"
           n <- getLine
           putStrLn $ "Hello " ++ reverse n
+
+infiniteRead :: IO ()
+infiniteRead = do line <- getLine
+                  if line == ""
+                    then return ()
+                    else do putStrLn $ reverse line
+                            infiniteRead
