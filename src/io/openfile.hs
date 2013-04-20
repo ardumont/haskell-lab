4bc36cfa2cad418fe111c9671c8d141e25c3585a
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2013-04-20 09:15:56 +0200

    Write the content of a file using writeFile :: FilePath -> String -> IO ()

--- src/io/openfile.hs
+++ src/io/openfile.hs
@@ -1,5 +1,7 @@
 module Openfile where
 
+import Data.Char (toUpper)
+
 -- openFile :: FilePath -> IOMode -> IO Handle
 -- ReadMode :: IOMode
 -- hGetContents :: Handle -> IO String
@@ -15,7 +17,19 @@ module Openfile where
 -- readFile :: FilePath -> IO String
 
 main :: IO ()
-main = displayContentOfFile "./resources/life"
+main =
+  do writeContentOfAFile "./resources/life" "./resources/life-to-upper"
+     displayContentOfFile "./resources/life-to-upper"
+
+-- *Openfile> main
+-- I KNOW *EXACTLY* WHAT YOU MEAN.
+-- LET ME TELL YOU WHY YOU'RE HERE.
+-- YOU'RE HERE BECAUSE YOU KNOW SOMETHING.
+-- WHAT YOU KNOW YOU CAN'T EXPLAIN, BUT YOU FEEL IT.
+-- YOU'VE FELT IT YOUR ENTIRE LIFE, THAT THERE'S SOMETHING WRONG WITH THE WORLD.
+-- YOU DON'T KNOW WHAT IT IS, BUT IT'S THERE, LIKE A SPLINTER IN YOUR MIND, DRIVING YOU MAD.
+-- IT IS THIS FEELING THAT HAS BROUGHT YOU TO ME.
+-- DO YOU KNOW WHAT I'M TALKING ABOUT?
 
 displayContentOfFile :: FilePath -> IO ()
 displayContentOfFile file = do contents <- readFile file
@@ -30,3 +44,21 @@ displayContentOfFile file = do contents <- readFile file
 -- You don't know what it is, but it's there, like a splinter in your mind, driving you mad.
 -- It is this feeling that has brought you to me.
 -- Do you know what I'm talking about?
+
+-- writeInAFile :: IO ()
+
+writeContentOfAFile :: FilePath -> FilePath -> IO ()
+writeContentOfAFile fileIn fileOut =
+  do contents <- readFile fileIn
+     writeFile fileOut (map toUpper contents)
+
+-- *Openfile> writeContentOfAFile "./resources/life" "./resources/lifeupper"
+-- *Openfile> displayContentOfFile "./resources/lifeupper"
+-- I KNOW *EXACTLY* WHAT YOU MEAN.
+-- LET ME TELL YOU WHY YOU'RE HERE.
+-- YOU'RE HERE BECAUSE YOU KNOW SOMETHING.
+-- WHAT YOU KNOW YOU CAN'T EXPLAIN, BUT YOU FEEL IT.
+-- YOU'VE FELT IT YOUR ENTIRE LIFE, THAT THERE'S SOMETHING WRONG WITH THE WORLD.
+-- YOU DON'T KNOW WHAT IT IS, BUT IT'S THERE, LIKE A SPLINTER IN YOUR MIND, DRIVING YOU MAD.
+-- IT IS THIS FEELING THAT HAS BROUGHT YOU TO ME.
+-- DO YOU KNOW WHAT I'M TALKING ABOUT?
