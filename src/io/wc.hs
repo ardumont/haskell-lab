2a32c411b19ef1535ae248a139f806e6a5437062
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2013-04-21 20:15:51 +0200

    Dealing with retrieving the filename from the IOError.

--- src/io/wc.hs
+++ src/io/wc.hs
@@ -3,14 +3,18 @@ module Main where
 --import System.IO
 import System.Environment (getArgs)
 import Control.Exception (catch)
-import System.IO.Error (isDoesNotExistError)
+import System.IO.Error (isDoesNotExistError,
+                        ioeGetFileName)
 
 main :: IO ()
 main = Control.Exception.catch countLines handlerExc
 
 handlerExc :: IOError -> IO ()
 handlerExc e
-  | isDoesNotExistError e = putStrLn "The file does not exist!"
+  | isDoesNotExistError e =
+    case ioeGetFileName e of
+      Just file -> putStrLn $ "'" ++ file ++ "' does not exist!"
+      Nothing   -> putStrLn "Oops, the file does not exist!"
   | otherwise             = ioError e
 
 countLines :: IO ()
