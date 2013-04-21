c5efb598d768c9b07b37824fdf6e244001188c8d
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2013-04-21 19:19:13 +0200

    A namespace to deal with copy of large files (using Data.ByteString.Lazy)

---
+++ src/io/copy.hs
@@ -0,0 +1,14 @@
+module Main where
+
+import System.Environment
+import qualified Data.ByteString.Lazy as B
+
+main :: IO ()
+main = do
+  (fileName1:fileName2:_) <- getArgs
+  copyFile fileName1 fileName2
+
+copyFile :: FilePath -> FilePath -> IO ()
+copyFile source dest = do
+  contents <- B.readFile source
+  B.writeFile dest contents
