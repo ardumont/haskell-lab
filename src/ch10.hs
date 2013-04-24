b2b63b79bea391e622f1987ca3ec0e81a4530660
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2013-04-24 17:46:02 +0200

    Using || instead of or

--- src/ch10.hs
+++ src/ch10.hs
@@ -7,7 +7,9 @@ t = Node (Node (Leaf 1) 2 (Leaf 3)) 4 (Node (Leaf 5) 6 (Leaf 7))
 
 occurs :: Eq a => Tree a -> a -> Bool
 occurs (Leaf n)     m = n == m
-occurs (Node l n r) m = or [(n == m), (occurs l m), (occurs r m)]
+occurs (Node l n r) m = (n == m) ||
+                        (occurs l m) ||
+                        (occurs r m)
 
 -- *Ch10> map (occurs t) [0..8]
 -- [False,True,True,True,True,True,True,True,False]
