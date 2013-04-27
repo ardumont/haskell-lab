ea683e1bbbf3dec7e42b8f9133577b3cb5435f3f
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2013-04-27 17:46:09 +0200

    I think i have the right one this time.

--- src/PropParser.hs
+++ src/PropParser.hs
@@ -14,61 +14,34 @@ import String (capitalize)
 --           | Equiv Prop Prop
 --           deriving Show
 
-propConst :: Parser Bool
+propConst :: Parser Prop
 propConst = do symbol "Const"
                b <- symbol "true" +++
                     symbol "True" +++
                     symbol "False" +++
                     symbol "false"
-               return $ read (capitalize b)
+               let bool = read (capitalize b) :: Bool in
+                 return $ Const bool
 
-pconst :: String -> Maybe Prop
-pconst s = case parse propConst s of
-  [(b, _)] -> Just (Const b)
-  _        -> Nothing
+-- *PropParsers> parse propConst "Const asdtrue"
+-- []
+-- *PropParsers> parse propConst "Const true"
+-- [(Const True,"")]
+-- *PropParsers> parse propConst "Const false"
+-- [(Const False,"")]
+-- *PropParsers> parse propConst "Const False"
+-- [(Const False,"")]
+-- *PropParsers> parse propConst "Const True"
+-- [(Const True,"")]
 
--- *PropParsers> pconst "Const true"
--- Just (Const True)
--- *PropParsers> pconst "Const True"
--- Just (Const True)
--- *PropParsers> pconst "Const false"
--- Just (Const False)
--- *PropParsers> pconst "Const False"
--- Just (Const False)
--- *PropParsers> pconst "Const nawak False"
--- Nothing
--- *PropParsers> pconst " nawak "
--- Nothing
-
-var :: Parser Char
+var :: Parser Prop
 var = do symbol "Var"
-         letter
+         l <- letter
+         return $ Var l
 
 -- *PropParsers> parse var "Var a"
--- [('a',"")]
--- *PropParsers> parse var "Var b"
--- [('b',"")]
--- *PropParsers> parse var "Var c"
--- [('c',"")]
-
-
-pvar :: String -> Maybe Prop
-pvar s = case parse var s of
-  [(v, _)] -> Just (Var v)
-  _        -> Nothing
-
--- *PropParsers> pvar "Var a"
--- Just (Var 'a')
--- *PropParsers> pvar "Var alskdfj"
--- Just (Var 'a')
--- *PropParsers> pvar "Var nothing"
--- Just (Var 'n')
--- *PropParsers> pvar "nawak"
--- Nothing
-
--- prop :: Parser Prop
--- prop = const +++ var
-
--- not :: Parser Prop
--- not = do symbol "Not"
---          prop
+-- [(Var 'a',"")]
+-- *PropParsers> parse var "nawak"
+-- []
+-- *PropParsers> parse var "Var abc"
+-- [(Var 'a',"bc")]
