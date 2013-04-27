79c25007ebd3d34ae0096158dd7a1a6b865e761f
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2013-04-27 18:39:47 +0200

    adding example

--- src/PropParser.hs
+++ src/PropParser.hs
@@ -60,17 +60,17 @@ propEval s = case parse prop s of
   [(p, "")] -> Just p
   _         -> Nothing
 
--- *PropParsers> propEval "! a & b"
--- Nothing
--- *PropParsers> propEval "! & a b"
--- Just (Not (And (Var 'a') (Var 'b')))
--- *PropParsers> propEval "! & | a b c"
--- Just (Not (And (Or (Var 'a') (Var 'b')) (Var 'c')))
--- *PropParsers> propEval "=> a b"
--- Just (Imply (Var 'a') (Var 'b'))
--- *PropParsers> propEval "<=> a b"
--- Just (Equiv (Var 'a') (Var 'b'))
--- *PropParsers> propEval "<=> a true"
--- Just (Equiv (Var 'a') (Const True))
--- *PropParsers> propEval "<=> a ! true"
--- Just (Equiv (Var 'a') (Not (Const True)))
+-- *PropParsers> propEval "f"
+-- Just (Const False)
+-- *PropParsers> propEval "f"
+-- Just (Const False)
+-- *PropParsers> propEval "! f"
+-- Just (Not (Const False))
+-- *PropParsers> propEval "! a"
+-- Just (Not (Var 'a'))
+-- *PropParsers> propEval "& ! a b"
+-- Just (And (Not (Var 'a')) (Var 'b'))
+-- *PropParsers> propEval "& ! a t"
+-- Just (And (Not (Var 'a')) (Const True))
+-- *PropParsers> propEval "& ! a <=> t f"
+-- Just (And (Not (Var 'a')) (Equiv (Const True) (Const False)))
