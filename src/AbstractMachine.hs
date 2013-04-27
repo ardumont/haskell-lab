bfff05403e0569108a19c6ece1e6fbf19cd1d556
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2013-04-27 20:05:28 +0200

    Adding multiplication to the abstract machine.

--- src/AbstractMachine.hs
+++ src/AbstractMachine.hs
@@ -1,22 +1,37 @@
 module AbstractMachine where
 
-data Expr = Val Int | Add Expr Expr
+data Expr = Val Int | Add Expr Expr | Mult Expr Expr deriving Show
 
 value:: Expr -> Int
 value (Val n)   = n
+value (Mult x y) = value x + value y
 value (Add x y) = value x + value y
 
+-- *AbstractMachine> value (Mult (Val 10) (Val 5))
+-- 15
+-- *AbstractMachine> value (Add (Val 10) (Mult (Val 10) (Val 5)))
+-- 25
+
+data Op = EVALM Expr | EVALA Expr | MUL Int | ADD Int deriving Show
+
 type Cont = [Op]
-data Op = EVAL Expr | ADD Int
 
 eval :: Expr -> Cont -> Int
-eval (Val n) c   = exec c n
-eval (Add x y) c = eval x (EVAL y : c)
+eval (Val n)    c = exec c n
+eval (Mult x y) c = eval x (EVALM y : c)
+eval (Add x y)  c = eval x (EVALA y : c)
 
 exec :: Cont -> Int -> Int
-exec [] n           = n
-exec (EVAL y : c) n = eval y (ADD n : c)
-exec (ADD n : c) m  = exec c (n + m)
+exec []            n = n
+exec (EVALM y : c) n = eval y (MUL n : c)
+exec (EVALA y : c) n = eval y (ADD n : c)
+exec (MUL m : c)   n = exec c (n * m)
+exec (ADD m : c)   n = exec c (n + m)
 
 val :: Expr -> Int
 val e = eval e []
+
+-- *AbstractMachine> val (Mult (Val 10) (Val 5))
+-- 50
+-- *AbstractMachine> val (Add (Mult (Val 10) (Val 5)) (Val 100))
+-- 150
