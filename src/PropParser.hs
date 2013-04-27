cacfa079e29c66a5d70ba17c797e247148ace1ca
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2013-04-27 16:34:36 +0200

    Creating the parser of Prop

---
+++ src/PropParser.hs
@@ -0,0 +1,34 @@
+module PropParsers where
+
+import Parsers
+
+-- data Prop = Const Bool
+--           | Var Char
+--           | Not Prop
+--           | And Prop Prop
+--           | Imply Prop Prop
+--           | Or Prop Prop
+--           | Equiv Prop Prop
+--           deriving Show
+
+const :: Parser String
+const = do symbol "Const"
+           symbol "true" +++
+             symbol "True" +++
+             symbol "False" +++
+             symbol "false"
+
+-- *PropParsers> parse const "Const true"
+-- [("true","")]
+-- *PropParsers> parse const "Const True"
+-- [("True","")]
+-- *PropParsers> parse const "Const False"
+-- [("False","")]
+-- *PropParsers> parse const "Const false"
+-- [("false","")]
+-- *PropParsers> parse const "Const nawak"
+-- []
+-- *PropParsers> parse const " nawak"
+-- []
+-- *PropParsers> parse const " True"
+-- []
