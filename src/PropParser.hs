f6afb1deae42518fad0212d846d944cfd1b54b2b
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2013-04-27 19:39:39 +0200

    Adding samples

--- src/PropParser.hs
+++ src/PropParser.hs
@@ -22,20 +22,56 @@ propConst = do b <- symbol "t" +++ symbol "f"
                  case bool of
                    Just v -> return $ Const v
 
+-- *PropParsers> parse propConst "t"
+-- [(Const True,"")]
+-- *PropParsers> parse propConst "f"
+-- [(Const False,"")]
+-- *PropParsers> parse propConst "a"
+-- []
+
 propVar :: Parser Prop
 propVar = do l <- token letter
              return $ Var l
+
+-- *PropParsers> parse propVar "a"
+-- [(Var 'a',"")]
+-- *PropParsers> parse propVar "t"
+-- [(Var 't',"")]
+-- *PropParsers> parse propVar "na"
+-- [(Var 'n',"a")]
+-- *PropParsers> parse propVar "1"
+-- []
 
 propNot :: Parser Prop
 propNot = do symbol "!"
              a <- prop
              return (Not a)
 
+-- *PropParsers> parse propNot "! a"
+-- [(Not (Var 'a'),"")]
+-- *PropParsers> parse propNot "! asldfsd"
+-- [(Not (Var 'a'),"sldfsd")]
+-- *PropParsers> parse propNot "! t"
+-- [(Not (Const True),"")]
+-- *PropParsers> parse propNot " t"
+-- []
+
 propAnd :: Parser Prop
 propAnd = do symbol "&"
              a <- prop
              b <- prop
              return (And a b)
+
+-- *PropParsers> parse propAnd "& a b"
+-- [(And (Var 'a') (Var 'b'),"")]
+-- *PropParsers> parse propAnd "& a ! b"
+-- [(And (Var 'a') (Not (Var 'b')),"")]
+-- *PropParsers> parse propAnd "& a ! t"
+-- [(And (Var 'a') (Not (Const True)),"")]
+-- *PropParsers> parse propAnd "& a ! t"
+-- [(And (Var 'a') (Not (Const True)),"")]
+-- *PropParsers> parse propAnd "& a "
+-- []
 
 propOr :: Parser Prop
 propOr = do symbol "|"
@@ -49,11 +85,21 @@ propImply = do symbol "=>"
                b <- prop
                return (Imply a b)
 
+-- *PropParsers> parse propImply "=> t ! & a c"
+-- [(Imply (Const True) (Not (And (Var 'a') (Var 'c'))),"")]
+-- *PropParsers> parse propImply "=> t "
+-- []
+
 propEquiv :: Parser Prop
 propEquiv = do symbol "<=>"
                a <- prop
                b <- prop
                return (Equiv a b)
+
+-- *PropParsers> parse propEquiv "<=> t "
+-- []
+-- *PropParsers> parse propEquiv "<=> t ! & a c"
+-- [(Equiv (Const True) (Not (And (Var 'a') (Var 'c'))),"")]
 
 propEval :: String -> (Maybe Prop)
 propEval s = case parse prop s of
