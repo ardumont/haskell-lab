c35243533f9f9f6f4f86d113deee57c31c1d45fe
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2013-04-29 14:32:57 +0200

    Starting the funset exercise.

---
+++ src/FunSet.hs
@@ -0,0 +1,21 @@
+module FunSet where
+
+type Set a = a -> Bool
+
+-- newEmpty :: Set a
+
+-- add :: Eq a => Set a -> a -> Set a
+
+-- contains ::  Set a -> a -> Bool
+
+-- singleton :: Eq a => a ->Set a
+
+-- union :: Set a -> Set a -> Set a
+
+-- intersect :: Set a -> Set a -> Set a
+
+-- diff :: Set a -> Set a -> Set a
+
+-- filter' :: (a -> Bool) -> Set a -> Set a
+
+-- remove :: Eq a => a -> Set a -> Set a
