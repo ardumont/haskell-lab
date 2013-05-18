789ea1b5024bcf474b0e453273dcee77d91e1f92
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2013-05-18 16:56:31 +0200

    simplification

--- src/primes/sieve.hs
+++ src/primes/sieve.hs
@@ -1,10 +1,9 @@
 module Sieve where
 
 prime :: Integral a => a -> [a]
-prime r =
-  sieve i []
+prime l =
+  sieve [2..l] []
   where
-    i                  = [2..r]
-    sieve []     range = range
-    sieve (x:xs) range =
-      sieve (filter (\ n -> n `mod` x /= 0) xs) (x:range)
+    sieve []     r = r
+    sieve (x:xs) r =
+      sieve (filter (\ n -> n `mod` x /= 0) xs) (x:r)
