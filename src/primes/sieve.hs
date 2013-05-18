3bbc1a9c0b8e0a547e245e3462b89bf06091ef6f
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2013-05-18 17:42:28 +0200

    Adding quickcheck to ensure the sieve is ok.

--- src/primes/sieve.hs
+++ src/primes/sieve.hs
@@ -1,5 +1,17 @@
 module Sieve where
 
+import Test.QuickCheck
+import Test.QuickCheck.All
+
+isPrime :: Integral a => a -> Bool
+isPrime n
+  | n <= 1 = False
+  | n == 2 = True
+  | otherwise =
+  all ( \ y -> n `mod` y /= 0) primes
+  where
+    primes = (2:[3,5..(floor . sqrt . fromIntegral) n])
+
 prime :: Integral a => a -> [a]
 prime l =
   sieve [2..l] []
@@ -7,3 +19,32 @@ prime l =
     sieve []     r = r
     sieve (x:xs) r =
       sieve (filter ( (/= 0) . flip (mod) x) xs) (x:r)
+
+-- *Sieve> filter isPrime [1..100] == (reverse . prime) 100
+-- True
+
+prop_prime :: Integer -> Bool
+prop_prime = (\ n -> all isPrime (prime n))
+
+-- -- -- adding
+main :: IO ()
+main = do
+  -- verboseCheckWith stdArgs { maxSuccess = 1000, maxSize = 5 } prop_prime
+  verboseCheckWith stdArgs { maxSuccess = 10000 } prop_prime
+  -- verboseCheckWith stdArgs prop_prime
+
+-- Passed:
+-- -43
+-- Passed:
+-- -23
+-- Passed:
+-- 20
+-- Passed:
+-- -21
+-- Passed:
+-- -36
+-- Passed:
+-- -18
+-- Passed:
+-- -75
+-- +++ OK, passed 10000 tests.
