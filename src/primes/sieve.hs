48dd340566fbcf8c8bdb89998fcf327a6c07f6c3
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2013-05-18 16:55:29 +0200

    Sieve of erathostenes

--- src/primes/sieve.hs
+++ src/primes/sieve.hs
@@ -1,11 +1,10 @@
 module Sieve where
 
-prime :: Integral a => [a] -> [a]
-prime r = sieve [2] r
-          where
-            sieve []  r = r
-            sieve [x] r =
-              let l = filter (\ n -> n `mod` x /= 0) r in
-              if (null l) || (length l) == (length r)
-              then r
-              else sieve [(head l)] $ x : (tail l)
+prime :: Integral a => a -> [a]
+prime r =
+  sieve i []
+  where
+    i                  = [2..r]
+    sieve []     range = range
+    sieve (x:xs) range =
+      sieve (filter (\ n -> n `mod` x /= 0) xs) (x:range)
