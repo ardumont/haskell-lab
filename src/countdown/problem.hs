a3f1f1397d1dee47ea695dd547504f578eb0a4c6
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2013-05-26 10:47:32 +0200

    Another measure from the chapter 11.

--- src/countdown/problem.hs
+++ src/countdown/problem.hs
@@ -4,10 +4,10 @@ module Problem where
 data Op = Add | Sub | Mul | Div deriving (Show)
 
 valid :: Op -> Int -> Int -> Bool
-valid Add x y = x <= y
-valid Sub x y = x > y
-valid Mul x y = x <= y && x /= 1 && y /= 1
-valid Div x y = y /= 1 && x `mod` y == 0
+valid Add _ _ = True
+valid Sub _ _ = True
+valid Mul _ _ = True
+valid Div x y = y /= 0 && x `mod` y == 0
 
 -- *Problem> valid Add 10 20
 -- True
@@ -225,3 +225,7 @@ isChoice (x:xs) l = elem x l && isChoice xs (remove1 x l)
 
 -- *Problem> length [ es | cs <- choices [1,3,7,10,25,50], es <- exprs cs, eval es /= []]
 -- 245644
+
+-- Modifying the valid functions
+-- *Problem> length [ es | cs <- choices [1,3,7,10,25,50], es <- exprs cs, eval es /= []]
+-- 10839369
