4c9e78bfbd7a19ebb50093223d5f6fdd007a4a2d
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2013-05-26 17:47:01 +0200

    fibs :: Int -> Integer

--- src/c12.hs
+++ src/c12.hs
@@ -1,7 +1,10 @@
 module C12 where
 
-fibs :: [Integer]
-fibs = 0:1:[ x+y | (x,y) <- zip fibs (tail fibs)]
+fibo :: [Integer]
+fibo = 0:1:[ x+y | (x,y) <- zip fibo (tail fibo)]
 
--- *C12> take 20 fibs
+-- *C12> take 20 fibo
 -- [0,1,1,2,3,5,8,13,21,34,55,89,144,233,377,610,987,1597,2584,4181]
+
+fibs :: Int -> Integer
+fibs n = last $ take n fibo
