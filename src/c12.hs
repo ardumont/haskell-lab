f312209cb156a6a7772bee5fe8b0fdaa42197876
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2013-05-26 17:40:20 +0200

    Using list comprehension

--- src/c12.hs
+++ src/c12.hs
@@ -1,6 +1,7 @@
 module C12 where
 
-fibs :: [Int]
-fibs = fibo [0,1]
-       where
-         fibo (x:y:xs) = x:fibo (y:x+y:xs)
+fibs :: [Integer]
+fibs = 0:1:[ x+y | (x,y) <- zip fibs (tail fibs)]
+
+-- *C12> take 20 fibs
+-- [0,1,1,2,3,5,8,13,21,34,55,89,144,233,377,610,987,1597,2584,4181]
