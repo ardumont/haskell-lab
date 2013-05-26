1febc9f627db1d1eda1d7bfb42213e421f363b4d
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2013-05-26 17:59:04 +0200

    Finding the first fibonacci number greater than 1000

--- src/c12.hs
+++ src/c12.hs
@@ -8,3 +8,13 @@ fibo = 0:1:[ x+y | (x,y) <- zip fibo (tail fibo)]
 
 fibs :: Int -> Integer
 fibs n = last $ take n fibo
+
+-- *C12> fibs 10
+-- 34
+-- *C12> fibs 100
+-- 218922995834555169026
+-- *C12> fibs 101
+-- 354224848179261915075
+
+-- *C12> head . dropWhile (<= 1000) $ fibo
+-- 1597
