ad38b102d104117a2fcad048e1070d06fe824b5e
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2013-05-29 19:55:53 +0200

    nodes :: Tree -> Int

--- src/tree/c12.hs
+++ src/tree/c12.hs
@@ -8,3 +8,10 @@ leaves (Node l r) = leaves l + leaves r
 
 -- *C12> leaves (Node (Node (Leaf 0) (Node (Leaf 1) (Leaf 2))) (Leaf 3))
 -- 4
+
+nodes :: Tree -> Int
+nodes (Leaf _) = 0
+nodes (Node l r) = 1 + nodes l + nodes r
+
+-- *C12> nodes (Node (Node (Leaf 0) (Node (Leaf 1) (Leaf 2))) (Leaf 3))
+-- 3
