69e50767b5c6ed1f6ee62f2d651e647527949f2d
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2013-06-01 10:46:30 +0200

    Adding the use case for negative factorial that renders -1.

--- src/functional-approach/ch1.hs
+++ src/functional-approach/ch1.hs
@@ -1,5 +1,16 @@
 module Ch1 where
 
 fact :: Int -> Int
-fact 0 = 1
-fact n = n * fact (n-1)
+fact n
+  | n < 0     = -1
+  | n == 0    = 1
+  | otherwise = n * fact (n-1)
+
+-- *Ch1> fact 0
+-- 1
+-- *Ch1> fact 1
+-- 1
+-- *Ch1> fact 5
+-- 120
+-- *Ch1> fact (-10)
+-- -1
