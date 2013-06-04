bb070c0cd684c3d5bad898c489967308238da768
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2013-06-04 09:45:11 +0200

    Adding function from the chapter 3 problem

--- src/functional-approach/ch3.hs
+++ src/functional-approach/ch3.hs
@@ -6,3 +6,14 @@ power x k = if k == 0
             else if (k `mod` 2) == 0
                  then power (x*x) (k `div` 2)
                  else x * power (x*x) (k `div` 2)
+
+prodsum :: (Eq a, Num a) => a -> a
+prodsum x = prod x + s x
+
+prod :: (Eq a, Num a) => a -> a
+prod 0 = 1
+prod n = n * prod (n-1)
+
+s :: (Eq a, Num a) => a -> a
+s 0 = 0
+s n = n + s (n-1)
