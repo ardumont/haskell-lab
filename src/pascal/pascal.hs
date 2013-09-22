f62e5f37b3444aa7621bc85d980eafa639a64955
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2013-09-22 09:50:36 +0200

    Adding samples

--- src/pascal/pascal.hs
+++ src/pascal/pascal.hs
@@ -5,3 +5,18 @@ triangle xs = tri xs [1]
               where tri :: [Int] -> [Int] -> [Int]
                     tri [x] r      = x:r
                     tri (x:y:ys) r = tri (y:ys) ((x+y):r)
+
+-- *Pascal> triangle [1]
+-- [1,1]
+-- *Pascal> triangle [1,1]
+-- [1,2,1]
+-- *Pascal> triangle [1,2,1]
+-- [1,3,3,1]
+-- *Pascal> triangle [1,3,3,1]
+-- [1,4,6,4,1]
+-- *Pascal> triangle [1,4,6,4,1]
+-- [1,5,10,10,5,1]
+-- *Pascal> triangle [1,5,10,10,5,1]
+-- [1,6,15,20,15,6,1]
+-- *Pascal> triangle [1,6,15,20,15,6,1]
+-- [1,7,21,35,35,21,7,1]
