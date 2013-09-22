64a77ffd9152586904583f6993d85004e104e26e
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2013-09-22 09:53:46 +0200

    Use less parens

--- src/pascal/pascal.hs
+++ src/pascal/pascal.hs
@@ -4,7 +4,7 @@ triangle :: [Int] -> [Int]
 triangle xs = tri xs [1]
               where tri :: [Int] -> [Int] -> [Int]
                     tri [x] r      = x:r
-                    tri (x:y:ys) r = tri (y:ys) ((x+y):r)
+                    tri (x:y:ys) r = tri (y:ys) $ (x+y):r
 
 -- *Pascal> triangle [1]
 -- [1,1]
