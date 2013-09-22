d88b147c81a03bdd8da19be61deede72ad4fb220
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2013-09-22 10:02:00 +0200

    triangleRow :: Int -> [Int]

--- src/pascal/pascal.hs
+++ src/pascal/pascal.hs
@@ -20,3 +20,27 @@ triangle xs = tri xs [1]
 -- [1,6,15,20,15,6,1]
 -- *Pascal> triangle [1,6,15,20,15,6,1]
 -- [1,7,21,35,35,21,7,1]
+
+triangleRow :: Int -> [Int]
+triangleRow 0 = [1]
+triangleRow 1 = [1,1]
+triangleRow n = tRow p (triangleRow p)
+                where p = n-1
+                      tRow :: Int -> [Int] -> [Int]
+                      tRow 0 r = r
+                      tRow m r = tRow (m-1) (triangle r)
+
+-- *Pascal> triangleRow 0
+-- [1]
+-- *Pascal> triangleRow 1
+-- [1,1]
+-- *Pascal> triangleRow 2
+-- [1,2,1]
+-- *Pascal> triangleRow 3
+-- [1,4,6,4,1]
+-- *Pascal> triangleRow 4
+-- [1,7,21,35,35,21,7,1]
+-- *Pascal> triangleRow 5
+-- [1,11,55,165,330,462,462,330,165,55,11,1]
+-- *Pascal> triangleRow 6
+-- [1,16,120,560,1820,4368,8008,11440,12870,11440,8008,4368,1820,560,120,16,1]
