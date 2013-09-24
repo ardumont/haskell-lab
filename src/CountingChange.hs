b37a4c0beffa57566b9f4b72c605a9b6d81f0b9a
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2013-09-24 20:16:14 +0200

    countingChange :: Int -> [Int] -> Int

--- src/CountingChange.hs
+++ src/CountingChange.hs
@@ -6,15 +6,23 @@ countchange :: Int -> [Int] -> Int
 countchange m c = countChange m $ (reverse . sort) c
 
 countChange :: Int -> [Int] -> Int
-countChange 0 _       = 1
-countChange _ []      = 0
-countChange m [x]     = countChange (m - x) [x]
-countChange m c@(x:_) = countChange m [x] + countChange (m - x) c
+countChange _ []       = 0
+countChange 0 _        = 1
+countChange m c@(x:xs) = if m < 0
+                         then 0
+                         else countChange m xs + countChange (m - x) c
 
--- 300 [100 20 10 5 1]
--- 100 + 100 + 100
--- 200 [100 20 10 5 1]
-
--- countchange 4 [1,2] -- 3
--- countchange 300 [5,10,20,50,100,200,500] -- 1022
--- countchange 301 [5,10,20,50,100,200,500] -- 0
+-- *CountingChange> countchange 300 [5,10,20,50,100,200,500] -- 1022
+-- 1022
+-- *CountingChange> countchange 301 [5,10,20,50,100,200,500] -- 1022
+-- 0
+-- *CountingChange> countchange 4 [5,10,20,50,100,200,500] -- 1022
+-- 0
+-- *CountingChange> countchange 4 [1,2] -- 1022
+-- 3
+-- *CountingChange> countchange 4 [] -- 1022
+-- 0
+-- *CountingChange> countchange 0 [1,2] -- 1022
+-- 1
+-- *CountingChange> countchange 100 [50,25,10,5,1]
+-- 292
