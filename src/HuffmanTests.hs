e85e3ee4b1aa8ff73ee7a198897a1a96fec00d0b
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2013-10-13 11:17:12 +0200

    TestLabel(label test) <=> label ~: test

--- src/HuffmanTests.hs
+++ src/HuffmanTests.hs
@@ -8,8 +8,8 @@ import Test.HUnit
 testWeight1 = 2 ~=? weight (Leaf 'c' 2)
 testWeight2 = 5 ~=? weight (Fork (Leaf 'c' 2) (Leaf 'd' 3) ['c', 'd'] 5)
 
-testWeights = TestList [TestLabel "testWeight1" testWeight1,
-                        TestLabel "testWeight2" testWeight2]
+testWeights = TestList ["testWeight1" ~: testWeight1,
+                        "testWeight2" ~: testWeight2]
 
 -- *HuffmanTests> runTestTT testWeights
 -- Cases: 2  Tried: 2  Errors: 0  Failures: 0
