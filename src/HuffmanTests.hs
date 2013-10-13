4a587f1c65af1779644501e234b67983f752a7f0
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2013-10-13 11:29:56 +0200

    tests on chars

--- src/HuffmanTests.hs
+++ src/HuffmanTests.hs
@@ -14,3 +14,12 @@ testWeights = TestList ["testWeight1" ~: testWeight1,
 -- *HuffmanTests> runTestTT testWeights
 -- Cases: 2  Tried: 2  Errors: 0  Failures: 0
 -- Counts {cases = 2, tried = 2, errors = 0, failures = 0}
+
+testChars1 = "c"  ~=? chars (Leaf 'c' 2)
+testChars2 = "cd" ~=? chars (Fork (Leaf 'c' 2) (Leaf 'd' 3) ['c', 'd'] 5)
+
+testChars  = TestList ["testChars1" ~: testChars1,
+                       "testChars2" ~: testChars2]
+
+-- Full tests
+tests = TestList [testWeights, testChars]
