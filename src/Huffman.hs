dd5e6a45c53df4fa07980c43c53165e85d529802
Antoine R. Dumont <ext.adumont@kyriba.com>
2013-10-18 15:26:06 +0200

    remove blank

--- src/Huffman.hs
+++ src/Huffman.hs
@@ -20,7 +20,7 @@ mkFork :: CodeTree -> CodeTree -> CodeTree
 mkFork l r = Fork l r  (chars l ++ chars r) $ weight l + weight r
 
 mkOrdered :: [(Char, Int)] -> [CodeTree]
-mkOrdered = map (uncurry Leaf) . sortBy (\a b -> snd a `compare`  snd b)
+mkOrdered = map (uncurry Leaf) . sortBy (\a b -> snd a `compare` snd b)
 
 singleton :: [CodeTree] -> Bool
 singleton [_] = True
