0196236a4597b81423183f5530ca772ff71a75b5
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2013-10-22 10:20:31 +0200

    Adding unit tests

--- src/Anagram.hs
+++ src/Anagram.hs
@@ -1,7 +1,7 @@
 module Anagram where
 
 import Data.List
---import Test.HUnit
+import Test.HUnit
 
 type Word = String
 
@@ -12,20 +12,48 @@ type Occurrences = [(Char, Int)]
 wordOccurrences :: Word -> Occurrences
 wordOccurrences = map (\x -> (head x, length x)) . group . sort
 
--- *Anagram> wordOccurrences "this is the last day on earth"
--- [(' ',6),('a',3),('d',1),('e',2),('h',3),('i',2),('l',1),('n',1),('o',1),('r',1),('s',3),('t',4),('y',1)]
--- *Anagram> wordOccurrences "pump it up"
--- [(' ',2),('i',1),('m',1),('p',3),('t',1),('u',2)]
-
 join :: String -> [String] -> [Char]
 join _ []      = []
 join d (hs:ss) = foldl (\s ns -> s ++ d ++ ns) hs ss
 
--- *Anagram> join ", " ["how do you do", "this is really exciting times", "functional programming is fun"]
--- "how do you do, this is really exciting times, functional programming is fun"
-
 sentenceOccurrences :: Sentence -> Occurrences
 sentenceOccurrences = wordOccurrences . (join "")
 
--- *Anagram> sentenceOccurrences ["this is the last day on earth", "pump it up"]
--- [(' ',8),('a',3),('d',1),('e',2),('h',3),('i',3),('l',1),('m',1),('n',1),('o',1),('p',3),('r',1),('s',3),('t',5),('u',2),('y',1)]
+-- TESTS
+
+testWordOccurrences1 :: Test.HUnit.Test
+testWordOccurrences1 = [(' ',6),('a',3),('d',1),('e',2),('h',3),('i',2),('l',1),('n',1),('o',1),('r',1),('s',3),('t',4),('y',1)]
+                       ~=?
+                       wordOccurrences "this is the last day on earth"
+
+testWordOccurrences2 :: Test.HUnit.Test
+testWordOccurrences2 = [(' ',2),('i',1),('m',1),('p',3),('t',1),('u',2)] ~=? wordOccurrences "pump it up"
+
+testWordOccurrencess :: Test.HUnit.Test
+testWordOccurrencess = TestList ["testWordOccurrences1" ~: testWordOccurrences1,
+                                 "testWordOccurrences2" ~: testWordOccurrences2]
+
+testJoin1 :: Test.HUnit.Test
+testJoin1 = "how do you do, this is really exciting times, functional programming is fun"
+            ~=?
+            join ", " ["how do you do", "this is really exciting times", "functional programming is fun"]
+
+testJoins :: Test.HUnit.Test
+testJoins = TestList ["testJoin1" ~: testJoin1]
+
+testSentenceOccurrences1 :: Test.HUnit.Test
+testSentenceOccurrences1 = [(' ',8),('a',3),('d',1),('e',2),('h',3),('i',3),('l',1),('m',1),('n',1),('o',1),('p',3),('r',1),('s',3),('t',5),('u',2),('y',1)]
+                           ~=?
+                           sentenceOccurrences ["this is the last day on earth", "pump it up"]
+
+testSentenceOccurrencess :: Test.HUnit.Test
+testSentenceOccurrencess = TestList [ "testSentenceOccurrences1" ~: testSentenceOccurrences1]
+
+-- Full tests
+tests :: Test.HUnit.Test
+tests = TestList [testWordOccurrencess,
+                  testJoins,
+                  testSentenceOccurrencess]
+
+main :: IO ()
+main = runTestTT tests >>= print
