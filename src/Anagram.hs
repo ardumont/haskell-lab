32b08d34e598d99fc512b2774841ea74f7a353a3
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2013-10-22 10:11:11 +0200

    sentenceOccurrences :: Sentence -> Occurrences

--- src/Anagram.hs
+++ src/Anagram.hs
@@ -23,3 +23,9 @@ join d (hs:ss) = foldl (\s ns -> s ++ d ++ ns) hs ss
 
 -- *Anagram> join ", " ["how do you do", "this is really exciting times", "functional programming is fun"]
 -- "how do you do, this is really exciting times, functional programming is fun"
+
+sentenceOccurrences :: Sentence -> Occurrences
+sentenceOccurrences = wordOccurrences . (join "")
+
+-- *Anagram> sentenceOccurrences ["this is the last day on earth", "pump it up"]
+-- [(' ',8),('a',3),('d',1),('e',2),('h',3),('i',3),('l',1),('m',1),('n',1),('o',1),('p',3),('r',1),('s',3),('t',5),('u',2),('y',1)]
