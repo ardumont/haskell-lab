3d3c648d91a1acd85488a22825b92245235d84c6
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2013-10-22 15:41:14 +0200

    combinations -> [a] -> [[a]]

---
+++ src/Combinations.hs
@@ -0,0 +1,28 @@
+module Combinations where
+
+import Test.HUnit
+
+combinations :: [a] -> [[a]]
+combinations = foldl comb [[]]
+        where comb acc e = acc ++ map ((:) e) acc
+
+testCombinations1 :: Test.HUnit.Test
+testCombinations1 = [[],[1],[2],[2,1],[3],[3,1],[3,2],[3,2,1]]
+                    ~=?
+                    combinations [1,2,3]
+
+testCombinations2 :: Test.HUnit.Test
+testCombinations2 = [[],[2],[4],[4,2],[6],[6,2],[6,4],[6,4,2],[8],[8,2],[8,4],[8,4,2],[8,6],[8,6,2],[8,6,4],[8,6,4,2]]
+                    ~=?
+                    combinations [2,4,6,8]
+
+testCombinationss :: Test.HUnit.Test
+testCombinationss = TestList ["testCombinations1" ~: testCombinations1,
+                              "testCombinations2" ~: testCombinations2]
+
+-- Full tests
+tests :: Test.HUnit.Test
+tests = TestList [testCombinationss]
+
+main :: IO ()
+main = runTestTT tests >>= print
