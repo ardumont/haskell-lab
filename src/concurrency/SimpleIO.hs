c295c09383fb416ac125977a4b47a29bad377935
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2013-12-08 09:13:00 +0100

    concurrentIO

--- src/concurrency/SimpleIO.hs
+++ src/concurrency/SimpleIO.hs
@@ -1,10 +1,21 @@
 module SimpleIO where
 
 import System.IO
+import Control.Concurrent
 
 sequentialIO :: IO ()
 sequentialIO =
-  mapM_ (hPutStr stdout) ["hello", " this", " will", " be", " outputed", " sequentially.\n"]
+  do mapM_ (hPutStr stdout) ["hello", " this", " will", " be", " outputed", " sequentially.\n"]
+     hPutStr stdout "Good bye!\n"
+
 
 -- *SimpleIO> sequentialIO
 -- hello this will be outputed sequentially.
+
+concurrentIO :: IO ()
+concurrentIO =
+  do forkIO sequentialIO
+     hPutStr stdout "The display of this string is non-deterministic."
+
+-- *SimpleIO> concurrentIO
+-- heTlhleo d itshpilsay  woifl lth ibse s toruitnpgu tiesd n osne-qdueetnetrimailnliys.t
