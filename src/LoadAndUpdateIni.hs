c165bb316e28dde7a08d6a9610a2b898d738b700
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2014-01-13 09:15:57 +0100

    Using IniConfig type

--- src/LoadAndUpdateIni.hs
+++ src/LoadAndUpdateIni.hs
@@ -159,7 +159,7 @@ setProperty k v m = Map.update (\l -> Just (l ++ [v])) k m
 -- *ParseIni2> setProperty "ExtensionDirs" ("Extension8", "test") $ fromString fullLinesSample
 -- fromList [("ExtensionDirs",[("Extension0","/home/tony/.mozilla/firefox/vfazausl.default/extensions/{DDC359D1-844A-42a7-9AA1-88A850A938A8}.xpi"),("Extension1","/home/tony/.mozilla/firefox/vfazausl.default/extensions/artur.dubovoy@gmail.com.xpi"),("Extension2","/home/tony/.mozilla/firefox/vfazausl.default/extensions/{a3a5c777-f583-4fef-9380-ab4add1bc2a2}.xpi"),("Extension3","/usr/share/mozilla/extensions/{ec8030f7-c20a-464f-9b0e-13a3a9e97384}/{2e1445b0-2682-11e1-bfc2-0800200c9a66}"),("Extension4","/usr/share/mozilla/extensions/{ec8030f7-c20a-464f-9b0e-13a3a9e97384}/ubufox@ubuntu.com"),("Extension5","/usr/lib/mozilla/extensions/{ec8030f7-c20a-464f-9b0e-13a3a9e97384}/online-accounts@lists.launchpad.net"),("Extension6","/home/tony/.mozilla/firefox/vfazausl.default/extensions/keysnail@mooz.github.com"),("Extension8","test")]),("ThemeDirs",[("Extension0","/usr/lib/firefox/browser/extensions/{972ce4c6-7e08-4474-a285-3208198ce6fd}")])]
 
-updateNewExtension :: Map.Map String [(String, t)] -> t -> Map.Map String [(String, t)]
+updateNewExtension :: IniConfig -> String -> IniConfig
 updateNewExtension iniProperties newExtensionValue =
   let nbExtensions = countProperties "ExtensionDirs" iniProperties
       newExtension = ("Extension" ++ (show nbExtensions), newExtensionValue)
@@ -168,7 +168,7 @@ updateNewExtension iniProperties newExtensionValue =
 -- *ParseIni2> updateNewExtension (fromString fullLinesSample) "some-value"
 -- fromList [("ExtensionDirs",[("Extension0","/home/tony/.mozilla/firefox/vfazausl.default/extensions/{DDC359D1-844A-42a7-9AA1-88A850A938A8}.xpi"),("Extension1","/home/tony/.mozilla/firefox/vfazausl.default/extensions/artur.dubovoy@gmail.com.xpi"),("Extension2","/home/tony/.mozilla/firefox/vfazausl.default/extensions/{a3a5c777-f583-4fef-9380-ab4add1bc2a2}.xpi"),("Extension3","/usr/share/mozilla/extensions/{ec8030f7-c20a-464f-9b0e-13a3a9e97384}/{2e1445b0-2682-11e1-bfc2-0800200c9a66}"),("Extension4","/usr/share/mozilla/extensions/{ec8030f7-c20a-464f-9b0e-13a3a9e97384}/ubufox@ubuntu.com"),("Extension5","/usr/lib/mozilla/extensions/{ec8030f7-c20a-464f-9b0e-13a3a9e97384}/online-accounts@lists.launchpad.net"),("Extension6","/home/tony/.mozilla/firefox/vfazausl.default/extensions/keysnail@mooz.github.com"),("Extension7","some-value")]),("ThemeDirs",[("Extension0","/usr/lib/firefox/browser/extensions/{972ce4c6-7e08-4474-a285-3208198ce6fd}")])]
 
-loadAndUpdateExtensionsWith :: FilePath -> String -> IO (Map.Map String [(String, String)])
+loadAndUpdateExtensionsWith :: FilePath -> String -> IO IniConfig
 loadAndUpdateExtensionsWith inputFilePath inputExtension =
   fromFilePath inputFilePath >>= \iniProperties -> return $ updateNewExtension iniProperties inputExtension
 
