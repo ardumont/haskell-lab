fbcdbdcf65f98ddc9d1e6aaa9e73b3aadf627f4d
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2014-02-03 15:35:35 +0100

    some nudge function

--- src/learn-you-a-haskell-for-great-good/own-types.hs
+++ src/learn-you-a-haskell-for-great-good/own-types.hs
@@ -20,3 +20,12 @@ surface (Circle _ r) = pi * r ^ 2
 -- 1256.6371
 -- *OurOwnTypes> surface $ Rectangle (Point 0 0) (Point 100 100)
 -- 10000.0
+
+nudge :: Shape -> Float -> Float -> Shape
+nudge (Rectangle (Point x1 y1) (Point x2 y2)) a b = Rectangle (Point (x1+a) (y1+b)) (Point (x2+a) (y2+b))
+nudge (Circle (Point x y) r) a b = Circle (Point (x+a) (y+b)) r
+
+-- *OurOwnTypes> nudge (Circle (Point 0 0) 10) 20 10
+-- Circle (Point 20.0 10.0) 10.0
+-- *OurOwnTypes> nudge (Rectangle (Point 0 0) (Point 100 100)) 20 20
+-- Rectangle (Point 20.0 20.0) (Point 120.0 120.0)
