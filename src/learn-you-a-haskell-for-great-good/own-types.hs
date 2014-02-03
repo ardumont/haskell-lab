9954b3129180b3f7425cb753f4f6d7874fe725b5
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2014-02-03 15:00:06 +0100

    Intermediary Point data type to represent the coordinate of a point

--- src/learn-you-a-haskell-for-great-good/own-types.hs
+++ src/learn-you-a-haskell-for-great-good/own-types.hs
@@ -1,6 +1,8 @@
 module OurOwnTypes where
 
-data Shape = Rectangle Float Float Float Float | Circle Float Float Float deriving Show
+data Point = Point Float Float deriving (Show)
+
+data Shape = Rectangle Point Point | Circle Point Float deriving (Show)
 
 -- Rectangle x1 y1 x2 y2
 -- (x1, y1) coordinate of the upper left corner
@@ -11,10 +13,10 @@ data Shape = Rectangle Float Float Float Float | Circle Float Float Float derivi
 -- r its radius
 
 surface :: Shape -> Float
-surface (Rectangle x1 y1 x2 y2) = (abs $ x2 - x1) * (abs $ y2 - y1)
-surface (Circle _ _ r) = pi * r ^ 2
+surface (Rectangle (Point x1 y1) (Point x2 y2)) = (abs $ x2 - x1) * (abs $ y2 - y1)
+surface (Circle _ r) = pi * r ^ 2
 
--- *OurOwnTypes> surface $ Circle 10 20 20
+-- *OurOwnTypes> surface $ Circle (Point 10 20) 20
 -- 1256.6371
--- *OurOwnTypes> surface $ Rectangle 0 0 100 100
+-- *OurOwnTypes> surface $ Rectangle (Point 0 0) (Point 100 100)
 -- 10000.0
