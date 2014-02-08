f246bf816023e85703386ef579fa089535960c26
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2014-02-08 12:56:31 +0100

    Add some more sample

--- src/Wifi.hs
+++ src/Wifi.hs
@@ -16,7 +16,8 @@ run fullCommand =
 
 -- *Wifi> run "nmcli con list"
 -- "NAME                      UUID                                   TYPE              TIMESTAMP-REAL                    \nAndroidAP-tony            68400207-92c9-4c8f-90b4-725b45c8359f   802-11-wireless   mar. 04 f\233vr. 2014 18:44:15 CET   \nZenika-1er                076684ca-6287-4625-bab6-524b865e185e   802-11-wireless   never                             \ntatooine                  deb87d57-aedc-46a8-8994-ce83c91ce522   802-11-wireless   sam. 08 f\233vr. 2014 12:44:56 CET   \n"
-
+-- *Wifi> run "nmcli --terse --fields name con list"
+-- "AndroidAP-tony\nZenika-1er\ntatooine\n"
 
 main :: IO ()
 main = do result <- run "nmcli con list"
