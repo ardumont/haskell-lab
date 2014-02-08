746c30552840cca4e22e85860f4e6033a3d32e54
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2014-02-08 13:32:20 +0100

    Add another sample

--- src/Wifi.hs
+++ src/Wifi.hs
@@ -20,6 +20,9 @@ run fullCommand =
 -- *Wifi> run "nmcli con list"
 -- ["NAME                      UUID                                   TYPE              TIMESTAMP-REAL                    ","AndroidAP-tony            68400207-92c9-4c8f-90b4-725b45c8359f   802-11-wireless   mar. 04 f\233vr. 2014 18:44:15 CET   ","Zenika-1er                076684ca-6287-4625-bab6-524b865e185e   802-11-wireless   never                             ","tatooine                  deb87d57-aedc-46a8-8994-ce83c91ce522   802-11-wireless   sam. 08 f\233vr. 2014 13:04:56 CET   "]
 
+-- Scan the wifi and return the ssid:signal
+-- *Wifi> run "nmcli --terse --fields ssid,signal dev wifi"
+-- ["'Livebox-0ff6':42","'tatooine':72"]
 main :: IO ()
 main = do result <- run "nmcli con list"
           mapM_ putStrLn result
