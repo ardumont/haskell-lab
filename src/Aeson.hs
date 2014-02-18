45496353e0adb1b88ad77fe533a89149448f9869
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2014-02-18 21:10:45 +0100

    Loading json from file or http url

--- src/Aeson.hs
+++ src/Aeson.hs
@@ -5,6 +5,8 @@ module Aeson where
 import qualified Data.Text as T
 import Data.Aeson
 import GHC.Generics
+import qualified Data.ByteString.Lazy as B
+import Network.HTTP.Conduit (simpleHttp)
 
 data Person =
   Person { firstName  :: !T.Text
@@ -15,3 +17,15 @@ data Person =
 
 instance FromJSON Person
 instance ToJSON Person
+
+jsonFile :: FilePath
+jsonFile = "../resources/pizza.json"
+
+getJSON :: IO B.ByteString
+getJSON = B.readFile jsonFile
+
+jsonURL :: String
+jsonURL = "http://daniel-diaz.github.io/misc/pizza.json"
+
+getJSONFromUrl :: IO B.ByteString
+getJSONFromUrl = simpleHttp jsonURL
