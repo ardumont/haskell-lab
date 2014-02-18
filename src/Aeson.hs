b989c6c4ff12a38f3319a13912570f1c9f92ef07
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2014-02-18 20:52:50 +0100

    Let the machine do the code geenration

--- src/Aeson.hs
+++ src/Aeson.hs
@@ -1,30 +1,17 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# OPTIONS_GHC -XDeriveGeneric #-}
 module Aeson where
 
 import qualified Data.Text as T
 import Data.Aeson
-import Control.Applicative
-import Control.Monad (mzero)
+import GHC.Generics
 
 data Person =
   Person { firstName  :: !T.Text
          , lastName   :: !T.Text
          , age        :: Int
          , likesPizza :: Bool
-           } deriving Show
+           } deriving (Show, Generic)
 
-instance FromJSON Person where
- parseJSON (Object v) =
-    Person <$> v .: "firstName"
-           <*> v .: "lastName"
-           <*> v .: "age"
-           <*> v .: "likesPizza"
- parseJSON _ = mzero
-
-instance ToJSON Person where
- toJSON (Person firstName lastName age likesPizza) =
-    object [ "firstName"  .= firstName
-           , "lastName"   .= lastName
-           , "age"        .= age
-           , "likesPizza" .= likesPizza
-             ]
+instance FromJSON Person
+instance ToJSON Person
