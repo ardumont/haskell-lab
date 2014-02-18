0059ee6cba1afc2f980bd29bde8f034444086119
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2014-02-18 21:20:20 +0100

    Loading json and decode it into a [Person] entry

--- src/Aeson.hs
+++ src/Aeson.hs
@@ -7,6 +7,7 @@ import Data.Aeson
 import GHC.Generics
 import qualified Data.ByteString.Lazy as B
 import Network.HTTP.Conduit (simpleHttp)
+import Control.Applicative
 
 data Person =
   Person { firstName  :: !Text
@@ -29,3 +30,15 @@ jsonURL = "http://daniel-diaz.github.io/misc/pizza.json"
 
 getJSONFromUrl :: IO B.ByteString
 getJSONFromUrl = simpleHttp jsonURL
+
+main :: IO ()
+main = do
+  -- Get JSON data and decode it
+  d <- (eitherDecode <$> getJSON) :: IO (Either String [Person])
+  -- If d is Left, the JSON was malformed.
+  -- In that case, we report the error.
+  -- Otherwise, we perform the operation of
+  -- our choice. In this case, just print it.
+  case d of
+    Left err -> putStrLn err
+    Right ps -> print ps
