10fd41f8d2b5d16ca5eabdf34e891a1482503496
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2014-02-21 12:57:44 +0100

    create module

--- src/persistent/LearnBasicPersistent.hs
+++ src/persistent/LearnBasicPersistent.hs
@@ -5,10 +5,12 @@
 {-# LANGUAGE QuasiQuotes       #-}
 {-# LANGUAGE TemplateHaskell   #-}
 {-# LANGUAGE TypeFamilies      #-}
-import           Control.Monad.IO.Class  (liftIO)
-import           Database.Persist
-import           Database.Persist.Sqlite
-import           Database.Persist.TH
+module LearnBasicPersistent where
+
+import Control.Monad.IO.Class  (liftIO)
+import Database.Persist
+import Database.Persist.Sqlite
+import Database.Persist.TH
 
 share [mkPersist sqlSettings, mkMigrate "migrateAll"] [persistLowerCase|
 Person
