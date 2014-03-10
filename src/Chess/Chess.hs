0069e9402150d2a02983237936eeae1f64ff58d9
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2014-03-10 09:09:23 +0100

    Determine if a knight can reach a particular position in 3 legal moves.

--- src/Chess/Chess.hs
+++ src/Chess/Chess.hs
@@ -7,3 +7,16 @@ moveKnight (c,r) = filter onBoard positions
   where positions = [(c+2,r-1),(c+2,r+1),(c-2,r-1),(c-2,r+1)
                     ,(c+1,r-2),(c+1,r+2),(c-1,r-2),(c-1,r+2)]
         onBoard (c', r') = c' `elem` [1..8] && r' `elem` [1..8]
+
+in3 :: KnightPos -> [KnightPos]
+in3 start = return start >>= moveKnight >>= moveKnight >>= moveKnight
+
+canReachIn3 :: KnightPos -> KnightPos -> Bool
+canReachIn3 start end = end `elem` in3 start
+
+-- *Chess> canReachIn3 (6,3) (8,2)
+-- True
+-- *Chess> canReachIn3 (6,3) (8,8)
+-- True
+-- *Chess> canReachIn3 (1,1) (8,8)
+-- False
