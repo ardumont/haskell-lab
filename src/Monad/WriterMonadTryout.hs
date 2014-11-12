8f1e3ffc4d1ac4bf80974b11d4163e0fba0756a9
Antoine R. Dumont <eniotna.t@gmail.com>
2014-11-12 08:40:42 +0100

    Implement a writer monad

--- src/Monad/WriterMonadTryout.hs
+++ src/Monad/WriterMonadTryout.hs
@@ -6,15 +6,34 @@ module Monad.WriterMonadTryout where
 -- execWriter :: Writer w a -> w
 -- runWriter :: Writer w a -> (a, w)
 
-import           Control.Monad.Writer
+import           Data.Monoid
 
-type MyWriter = Writer [Int] String
+-- Definition
 
-example :: MyWriter
+newtype MWriter w a = MWriter { runMWriter :: (a, w) }
+
+instance Monoid w => Monad (MWriter w) where
+  return a = MWriter (a, mempty)
+  m >>= k  = MWriter $ let
+      (a, w)  = runMWriter m
+      (b, w') = runMWriter (k a)
+      in (b, w `mappend` w')
+
+execMWriter :: MWriter w a -> w
+execMWriter m = snd (runMWriter m)
+
+mtell :: w -> MWriter w ()
+mtell w = MWriter ((), w)
+
+-- Sample
+
+type MyMWriter = MWriter [Int] String
+
+example :: MyMWriter
 example  = do
-  tell [1..5]
-  tell [5..10]
+  mtell [1..5]
+  mtell [5..10]
   return "foo"
 
 output :: (String, [Int])
-output = runWriter example
+output = runMWriter example
