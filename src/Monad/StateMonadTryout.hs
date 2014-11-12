0d1a81653271a70946194f70250db840c6414fd2
Antoine R. Dumont <eniotna.t@gmail.com>
2014-11-12 17:18:49 +0100

    Implement a state monad

--- src/Monad/StateMonadTryout.hs
+++ src/Monad/StateMonadTryout.hs
@@ -1,4 +1,4 @@
--- | Trying to understand by practicing on a small ReaderMonad sample
+-- | Trying to understand by practicing on a small StateMonad sample
 
 module Monad.StateMonadTryout where
 
@@ -6,13 +6,35 @@ module Monad.StateMonadTryout where
 -- evalState :: State s a -> s -> a
 -- execState :: State s a -> s -> s
 
-import           Control.Monad.State
+newtype MState s a = MState { mrunState :: s -> (a, s) }
 
-test :: State Int Int
+instance Monad (MState s) where
+  return a = MState $ \s -> (a, s)
+
+  MState act >>= k = MState $ \s ->
+    let (a, s') = act s
+    in mrunState (k a) s'
+
+mget :: MState s s
+mget = MState $ \s -> (s, s)
+
+mput :: s -> MState s ()
+mput s = MState $ const ((), s)
+
+mmodify :: (s -> s) -> MState s ()
+mmodify f = mget >>= \x -> mput (f x)
+
+mevalState :: MState s a -> s -> a
+mevalState act = fst . mrunState act
+
+mexecState :: MState s a -> s -> s
+mexecState act = snd . mrunState act
+
+test :: MState Int Int
 test = do
-  put 3
-  modify (+1)
-  get
+  mput 3
+  mmodify (+1)
+  mget
 
 main :: IO ()
-main = print $ execState test 0
+main = print $ mexecState test 0
